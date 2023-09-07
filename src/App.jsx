6a6853208a09b1e42694e093c4c38fff931f6ef2
nikkoro <nikodemdomaracki@gmail.com>
2023-09-07 16:56:00 +0200

    feat: smooth scroll on route change

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,28 @@ import { Watched } from "./components/Watched";
 import { Add } from "./components/Add";
 import "./lib/font-awesome/css/all.min.css";
 import { GlobalProvider } from "./context/GlobalState";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
-    return (
-        <GlobalProvider>
-            <Router>
-                <Header />
+  return (
+    <GlobalProvider>
+      <Router>
+        <ScrollToTop />
+        <Header />
 
-                <Switch>
-                    <Route exact path="/">
-                        <Watchlist />
-                    </Route>
-                    <Route exact path="/watched">
-                        <Watched />
-                    </Route>
-                    <Route exact path="/add">
-                        <Add />
-                    </Route>
-                </Switch>
-            </Router>
-        </GlobalProvider>
-    );
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route exact path="/watched">
+            <Watched />
+          </Route>
+          <Route exact path="/add">
+            <Add />
+          </Route>
+        </Switch>
+      </Router>
+    </GlobalProvider>
+  );
 }
 
 export default App;
