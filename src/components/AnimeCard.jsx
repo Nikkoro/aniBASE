8ea9bd778fd7f8daa8211dc8a6d9cce711264246
nikkoro <nikodemdomaracki@gmail.com>
2023-09-07 16:44:00 +0200

    chore: change substring to text overflow

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -4,10 +4,10 @@ import { AnimeControls } from "./AnimeControls";
 export const AnimeCard = ({ anime, type }) => {
   return (
     <div className="anime-card">
-      {anime.title.length <= 20 ? (
-        <h3 className="titles"> {anime.title.substring(0, 20)}</h3>
+      {anime.title.length ? (
+        <h3 className="titles"> {anime.title}</h3>
       ) : (
-        <h3 className="titles">{anime.title.substring(0, 17) + "..."}</h3>
+        <h3 className="titles">No title</h3>
       )}
       <div className="overlay"></div>
       <a href={anime.url} target="_blank">
